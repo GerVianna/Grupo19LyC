578822e09bf0930abe0313a1155c77ce74151ce3
Facundo Mediotte <facumediotte@gmail.com>
2020-11-08 22:33:26 -0300

    Revert "Agregando cambios tercetos"
    This reverts commit 7f92e085a65307eb5847772bcdf20e1c7e25ba2c.

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ flex Lexico.l
 bison -dyv Sintactico.y
 gcc lex.yy.c y.tab.c -o compilador
 gcc lex.yy.c y.tab.c -o Ejecutable.exe
-./compilador prueba3.txt
+./compilador prueba2.txt
 rm lex.yy.c
 rm y.output
 rm compilador
