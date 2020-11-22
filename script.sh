54ecfe792c554ef8841892c67867aab960f9e977
Facundo Mediotte <fmediotte@technisys.com>
2020-11-22 15:56:23 -0300

    Arreglo prueba.txt

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ flex Lexico.l
 bison -dyv Sintactico.y
 gcc lex.yy.c y.tab.c -o compilador
 gcc lex.yy.c y.tab.c -o Segunda.exe
-./compilador prueba2.txt
+./compilador prueba.txt
 rm lex.yy.c
 rm y.output
 rm compilador
